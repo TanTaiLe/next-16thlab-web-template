a585f5b9dcf828e50cb8a78aee7d00c1fcb931f8
TanTaiLe <33871424+TanTaiLe@users.noreply.github.com>
2025-09-08 16:11:04 +0700

    Fix: Add suppress to remove warning

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+
+const originalError = console.error;
+console.error = (...args) => {
+  if (typeof args[0] === "string" && args[0].includes("ReactDOMTestUtils.act")) {
+    return; // suppress act warning
+  }
+  originalError(...args);
+};
